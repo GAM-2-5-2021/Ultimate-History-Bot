826279b3eafc61dd92de8349829551b22c64348a
BrainCro <57328197+BrainCro@users.noreply.github.com>
2021-05-20 14:52:24 +0200

    Add files via upload

--- UHB.py
+++ UHB.py
@@ -2,11 +2,23 @@ import discord
 from discord.ext import commands
 with open('token.txt','r') as file:
 	token=file.read()
+with open('bitke.txt','r') as file:
+    bitke=list(file.read().split('\n'))
+with open('vladari.txt','r',encoding='UTF-8') as file:
+    vladari=list(file.read().split('\n'))
+vi=['Napoleon','LujXIV','LudovikVeliki']
+bi=['Borodino','Caporreto','Kursk']
 client=commands.Bot(command_prefix='_')
 @client.event
 async def on_ready():
-    print("Spreman!")
+    print(bitke)
 @client.command()
-async def Borodino(ctx):
-    await ctx.send("Bitka kod Borodina se dogodila 1813. godine")
+async def bitka(ctx,*,bit):
+    await ctx.send(bitke[bi.index(bit)])
+@client.command()
+async def Vladar(ctx,*,vit):
+    await ctx.send(vladari[vi.index(vit)])
+@client.command()
+async def dostupnebitke(ctx):
+    await ctx.send(bi)
 client.run(token)
