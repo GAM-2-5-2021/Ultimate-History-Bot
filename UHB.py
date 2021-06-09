69b89490476e9ba59a0984263c836f62ea9af4a6
BrainCro <57328197+BrainCro@users.noreply.github.com>
2021-06-09 22:50:41 +0200

    Add files via upload

--- UHB.py
+++ UHB.py
@@ -2,16 +2,16 @@ import discord
 from discord.ext import commands
 with open('token.txt','r') as file:
 	token=file.read()
-with open('bitke.txt','r') as file:
+with open('bitke.txt','r',encoding='UTF-8') as file:
     bitke=list(file.read().split('\n'))
 with open('vladari.txt','r',encoding='UTF-8') as file:
     vladari=list(file.read().split('\n'))
-vi=['Napoleon','LujXIV','LudovikVeliki']
-bi=['Borodino','Caporreto','Kursk']
+vi=['Napoleon','LujXIV','LudovikVeliki','SulejmanVeličanstveni','FranjoJosip']
+bi=['Borodino','Caporreto','Kursk','Mohačka','Lepand']
 client=commands.Bot(command_prefix='_')
 @client.event
 async def on_ready():
-    print(bitke)
+    print('Spreman!')
 @client.command()
 async def bitka(ctx,*,bit):
     await ctx.send(bitke[bi.index(bit)])
@@ -21,4 +21,11 @@ async def Vladar(ctx,*,vit):
 @client.command()
 async def dostupnebitke(ctx):
     await ctx.send(bi)
+@client.command()
+async def Vladari(ctx):
+    await ctx.send(vi)
+@client.command()
+async def pomoć(ctx):
+    await ctx.send('bitka,Vladar,dostupnebitke,Vladari')
+#@client.command()
 client.run(token)
